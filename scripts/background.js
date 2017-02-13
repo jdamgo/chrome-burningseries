b62d45da2e0c7586693c3c372cadc58ebd856140
jdamgo <jdamgo@users.noreply.github.com>
2017-02-13 16:46:29 +0100

    RESET

--- scripts/background.js
+++
@@ -1,4 +0,0 @@
-
-chrome.commands.onCommand.addListener(command => {
-  console.log("onCommand event received for message: ", command)
-})
--- scripts/database.js
+++
@@ -1,7 +0,0 @@
-
-burningseries.data = {
-  _add_imdb(imdb) {
-  },
-
-  add(...args) { return this._add_imdb(args) },
-}
